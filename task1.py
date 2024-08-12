8476737ed5b9918b8c1694f1d85403ea6a4fa8ad
NurAlam19 <138637931+NurAlam19@users.noreply.github.com>
2024-08-12 07:18:39 +0000

    task 1 selesai

---
+++ task1.py
@@ -0,0 +1,21 @@
+import pygame
+import PIL
+import cv2
+import moviepy.editor as mp
+import pydub
+import tkinter as tk 
+from importlib.metadata import version
+import pkg_resources
+
+def check_installation():
+    try:
+        pygame.init()
+        print("✅ Pygame version:", pygame.__version__)
+        print("✅ Pillow version:", PIL.__version__)
+        print("✅ OpenCV version:", cv2.__version__)
+        print("✅ MoviePy version:", version("moviepy"))
+        print("✅ Pydub version:", pkg_resources.get_distribution("pydub").version)
+    except Exception as e:
+        print("❌ Error:", e)
+
+check_installation()
